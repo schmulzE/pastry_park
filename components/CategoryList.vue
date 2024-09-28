1c414cbbd7c4d3460c28ddc538f144c23f8da22b
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-09-28 17:42:48 +0100

    implemented function to sort recipes and automatically fetch first recipe of category if found

--- components/CategoryList.vue
+++ components/CategoryList.vue
@@ -1,106 +1,190 @@
 <script lang="ts" setup>
+import { Types } from 'mongoose';
 import { IRecipe } from '~/types';
+// import { useScreenDetection } from '~/composables/useScreenDetection'
 
+// const { isDesktop, redirectIfMobile } = useScreenDetection();
+const route = useRoute();
+const router = useRouter();
+const currentTab = ref<number | null>(null);
 const props = defineProps({
   recipes: {
     type: Array<IRecipe>,
     required: true
+  },
+  category: {
+    type: String,
+    required: false
   }
 })
-interface ICategory {
-  id: string;
-  lists: IRecipe[]
-}
 
 const pastries = [  
-  'Shortcrust Pastry', 
-  'Puff Pastry', 
-  'Choux Pastry', 
-  'Phyllo (Filo) Pastry', 
-  'Sponge Pastry', 
-  'Yeast-Risen Pastry',
-  'Fried Pastry',
-  'Savory Pastries',
-  'Sweet Pastries',
-  'Decorative Pastries'
+  'Shortcrust', 
+  'Puff', 
+  'Choux', 
+  'Phyllo(Filo)', 
+  'Sponge', 
+  'Yeast-Risen',
+  'Fried',
+  'Savory',
+  'Sweet',
+  'Decorative',
+  'Fritters',
+  'Doughnuts',
+  'Batttered'
 ]
 
-const showbutton  = ref(false);
-const loading = ref(false);
-const showModal = ref(false);
-const confirm = ref(false);
-const recipeToDeleteId = ref<string | null>(null);
+function convertToTabs(pastries: string[], data: IRecipe[]) {
+  const tabs: { pastry: string; lists: { id?: Types.ObjectId; title: string; category: string;}[] }[] = [];
 
-const showModalHandler = (itemId: string) => {
-  recipeToDeleteId.value = itemId;
-  showModal.value = true;
-  confirm.value = false; // Reset confirmation
-}
-const sortbyCategory = (name: string) => {
-  return props.recipes.filter((recipe) => recipe.category == name)
-}
-const category: ICategory = (Object as any).assign.apply(null, pastries.map(x => ({ [x]: { 'name': x, 'lists': sortbyCategory(x)} })));
+  pastries.forEach((pastry: string) => {
+    const pastryData = data?.filter((item: { category: string; }) => item.category === pastry.toLowerCase());
+    tabs.push({
+      pastry: pastry,
+      lists: pastryData?.map((item) =>({ id: item._id, title: item.title, category: item.category }))
+    });
+  });
 
-const deleteRecipe = async() => {
-  try {
-    loading.value = true;
-    await $fetch(`/recipe/${recipeToDeleteId.value}`, {
-      method: "DELETE",
-    })
-  } catch (error) {
-    console.log(error)
-  }finally {
-    recipeToDeleteId.value = null
-    loading.value = false
-    showModal.value = false
+  return tabs;
+}
+
+
+const selectTab = (index: number, name: string) => {
+  currentTab.value = index;
+  localStorage.setItem('currentTab', index.toString())
+  localStorage.setItem('category', name);
+  // router.replace(`/browse/${name.toLowerCase()}`);
+  const selectedTab = tabs[index];
+  // Automatically navigate to the first item if it exists
+  if (selectedTab.lists.length > 0) {
+    const firstItem = selectedTab.lists[0];
+    router.replace(`/browse/${firstItem.category}/${firstItem.title.replaceAll(' ', '-').toLowerCase()}/${firstItem.id}`);
+  } else {
+    router.replace(`/browse/${name.toLowerCase()}`);
   }
+
 }
+
+// Function to handle incoming routes
+const handleIncomingRoute = () => {
+  const incomingCategory = route.params.pastry?.toString().toLowerCase();
+
+  // Find the tab based on the incoming category
+  const matchedTabIndex = tabs.findIndex(
+    tab => tab.pastry.toLowerCase() === incomingCategory
+  );
+
+  if (matchedTabIndex !== -1) {
+    currentTab.value = matchedTabIndex;
+
+    // If there's a specific recipe in the URL, don't override it
+    if (!route.params.recipe) {
+      const selectedTab = tabs[matchedTabIndex];
+      // Automatically navigate to the first item if it exists
+      if (selectedTab.lists.length > 0) {
+        const firstItem = selectedTab.lists[0];
+        router.replace(`/browse/${firstItem.category}/${firstItem.title.replaceAll(' ', '-').toLowerCase()}/${firstItem.id}`);
+      }
+    }
+  }
+};
+
+const nuxtApp = useNuxtApp();
+const isMobile = nuxtApp.$isMobile;
+
+onMounted(() => {
+  // Handle initial page load (e.g., coming from another page)
+  if(!isMobile){
+    handleIncomingRoute();
+  }
+
+  const storedTab = localStorage.getItem('currentTab')
+  if (storedTab !== null) {
+    currentTab.value = parseInt(storedTab, 10)
+  }
+})
+
+watch(route, () => {
+  // Handle route changes when the user navigates to a new category
+  // handleIncomingRoute();
+  if(!isMobile){
+    handleIncomingRoute();
+  }
+});
+
+const tabs = convertToTabs(pastries, props.recipes);
+
+// Computed property to filter recipes based on the selected filter;
+const sortRecipes = (criteria: string) => {
+  switch (criteria) {
+    case 'alphabetical':
+      return [...props.recipes].sort((a, b) => a.title.localeCompare(b.title));
+    case 'recent':
+      // return [...props.recipes].sort((a, b) => new Date(b.createdAt!) - new Date(a.createdAt!));
+    case 'rating':
+      return [...props.recipes].sort((a, b) => b.rating! - a.rating!);
+    default:
+      return props.recipes;
+  }
+};
+
+
 </script>
 
 <template>
-  <ul class="menu bg-base-100 lg:bg-gray-100 w-full rounded-box lg:h-auto h-full">
-    <li v-for="(pastry, index) in Object.values(category)" :key="index">
-      <details open>
-        <summary class="font-medium">{{ pastry.name }}</summary>
+  <div class="flex flex-row w-full h-full font-sans">
+    <div class="border-r border-gray-300 w-full md:w-36 lg:w-36 py-2 px-4 md:px-0 lg:px-0">
+      <ul>
+        <li 
+        v-for="(tab, index) in tabs" 
+        :key="index" 
+        class="text-lg md:text-sm lg:text-sm p-2 flex justify-between hover:bg-gray-300 cursor-pointer border-b md:border-0 lg:border-0" 
+        :class="currentTab === index && 'md:bg-gray-700 lg:bg-gray-700 md:text-white lg:text-white hover:bg-gray-700'"
+        @click="selectTab(index, tab.pastry)"
+        >
+          <NuxtLink :to="`/browse/${tab.pastry}`">{{ tab.pastry }}</NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div class="text-sm w-56 py-2 hidden md:block lg:block">
+      <div class="dropdown w-full">
+        <div tabindex="0" role="button" class="p-[8px] font-medium hover:bg-gray-300 border-b border-gray-300 flex items-center">
+          Sort by A-Z 
+          <i class="i-prime-sort-alt text-lg"></i>
+        </div>
+        <ul tabindex="0" class="dropdown-content z-[1] menu shadow bg-base-100 rounded-none w-full">
+          <li><a @click="sortRecipes('alphabetical')">Alphabetical</a></li>
+          <li><a @click="sortRecipes('recent')">Recent</a></li>
+          <li><a @click="sortRecipes('rating')">Rating</a></li>
+        </ul>
+      </div>
+      <div v-if='currentTab !== null'>
         <ul>
-          <li class="flex flex-row" v-for="list in pastry.lists" :key="list._id" @mouseenter="showbutton= true"  @mouseleave="showbutton=false" >
-            <NuxtLink class="flex-1" :to="`/browse/${list.category.replaceAll(' ', '-').toLowerCase()}/${list._id}`">
-              <span class="capitalize">{{ list.title }}</span>
+          <li v-for="(pastry, index) in tabs[currentTab].lists" :key="index">
+            <NuxtLink 
+            class="flex-1 w-full hover:bg-gray-300 p-2 inline-block font-sans" 
+            :to="`/browse/${pastry.category}/${pastry.title.replaceAll(' ', '-').toLowerCase()}/${pastry.id}`"
+            >
+              <span class="capitalize">{{ pastry.title }}</span>
             </NuxtLink> 
-            <div class="dropdown dropdown-bottom dropdown-end hover:bg-transparent ">
-              <i tabindex="0" role="button" class="i-ph-dots-three-vertical-bold"></i>
-              <ul tabindex="0" class="dropdown-content menu z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li><NuxtLink :to="`/recipe/edit/${list.category.replaceAll(' ', '-').toLowerCase()}/${list._id}`">Edit</NuxtLink></li>
-                <li><a @click="showModalHandler(list._id)">Delete</a></li>
-              </ul>
-            </div>
           </li>
         </ul>
-      </details>
-    </li>
-    <div v-show="showModal" class="flex items-center justify-center fixed left-0 bottom-0 w-full h-full bg-black/[0.6] z-50">
-    <div class="bg-white rounded-lg w-1/2 max-w-lg">
-      <div class="flex flex-col items-start p-4">
-        <div class="flex items-center w-full">
-          <div class="text-gray-900 font-medium text-2xl capitalize">delete recipe</div>
-            <svg  @click="showModal = false" class="ml-auto fill-current text-gray-700 w-6 h-6 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
-            <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/>
-            </svg>
-          </div>
-          <hr/>
-            <div class="mt-6">Deleting a recipe cannot be undone. Are you sure you want to permanently delete it?</div>
-          <hr/>
-          <div class="ml-auto flex space-x-4 mt-4">
-            <button @click="showModal = false" class="btn btn-sm btn-outline border-[#4b371c] bg-white text-[#4b371c] rounded-none">
-              Cancel
-            </button>
-            <button @click="deleteRecipe()" type="button" class="btn btn-sm bg-[#4b371c] hover:bg-[#4b371c] text-white rounded-none">
-              <span  v-if="loading" class="loading loading-spinner"></span>
-              Confirm
-            </button>
-        </div>
       </div>
+      <NuxtLink 
+      :to="`/${category}/add-recipe`" 
+      class="p-2 text-lg hover:bg-gray-300 border-y border-gray-300 flex items-center mt-4 font-semibold capitalize"
+      >
+        add recipe
+      </NuxtLink>
     </div>
   </div>
-  </ul>
 </template>
+
+<style scoped>
+.router-link-active {
+  @apply bg-gray-700 text-white;
+}
+.router-link-active:hover {
+  @apply bg-gray-700;
+}
+</style>
