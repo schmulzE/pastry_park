17e251fae084dfafd0b9ab201cc13a356ad1a775
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-09-30 23:29:34 +0100

    Add recipe retrieval by shared link ID with error handling -Implement fetching a recipe by linkId from the request parameters- Return a 400 error if the linkId is missing -Return a 404 error if the recipe is not found in the database -Add a 500 error for handling server issues during recipe fetching- Log errors for better debugging

---
+++ server/routes/recipe/by-link/[id].get.ts
@@ -0,0 +1,34 @@
+import { Recipe } from '~/server/models/recipe';  // Adjust this import based on your actual model location
+
+export default defineEventHandler(async (event) => {
+  const linkId = event.context.params?.id;
+
+  // console.log('id',linkId);
+
+  if (!linkId) {
+    throw createError({
+      statusCode: 400,
+      message: 'shared link is required'
+    })
+  }
+
+  try {
+    const recipe = await Recipe.findOne({ _id: linkId })
+
+    if (!recipe) {
+      throw createError({
+        statusCode: 404,
+        message: 'Recipe not found'
+      })
+    }
+
+    return recipe;
+
+  } catch (error) {
+    console.error('Error fetching recipe:', error)
+    throw createError({
+      statusCode: 500,
+      message: 'Error fetching recipe'
+    })
+  }
+})
