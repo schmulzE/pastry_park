a371e7a6369076d4f092d7e3ab101c4b99254f7a
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-10-01 12:59:41 +0100

    updated tailwind styling

--- components/Tab.vue
+++ components/Tab.vue
@@ -3,24 +3,24 @@
 </script>
 
 <template>
-  <div class="sticky top-0">
+  <div class="font-source sticky top-0">
     <div class="flex justify-between p-4">
       <button class="text-sm capitalize">back</button>
       <button class="text-sm capitalize">save</button>
     </div>
     
-    <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-        <ul class="flex flex-wrap justify-center ">
-            <li class="mr-9 text-center">
-                <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 uppercase">intro</a>
-            </li>
-            <li class="mr-9 text-center align-middle">
-                <a href="#" class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 uppercase" aria-current="page">ingredient</a>
-            </li>
-            <li class="mr-9 text-center self-center align-middle">
-                <a href="#" class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 uppercase">steps</a>
-            </li>
-        </ul>
+    <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 ">
+      <ul class="flex flex-wrap justify-center">
+        <li class="mr-9 text-center">
+          <a class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 uppercase">intro</a>
+        </li>
+        <li class="mr-9 text-center align-middle">
+          <a class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active uppercase" aria-current="page">ingredient</a>
+        </li>
+        <li class="mr-9 text-center self-center align-middle">
+          <a class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 uppercase">steps</a>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
