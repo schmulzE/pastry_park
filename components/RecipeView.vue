d59d4635c9453bf793dbe2639dfe44d31f55d86c
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-10-01 12:26:16 +0100

    Enhance RecipeDetail component -Add TypeScript props for recipe and ingredients -Implement ingredient selection functionality -Add show more/less toggle for long descriptions -Display recipe details including title, description, image, and cooking timesRender ingredients list and recipe steps -Include YouTube video embed if URL is provided -Improve responsive layout for different screen sizes

--- components/RecipeView.vue
+++ components/RecipeView.vue
@@ -1,52 +1,81 @@
 <script lang="ts" setup>
-import { IRecipe } from '~/types';
-const recipeStore = useRecipeStore()
-const props = defineProps(['recipe'])
+import { parseVulgars } from 'vulgar-fractions';
+import { IRecipeIngredient, IRecipe } from '~/types';
+import { convertToFraction } from '@/utils/fractionConverter';
 
+const props = defineProps<{
+  recipe: IRecipe,
+  recipeIngredients: IRecipeIngredient[],
+}>()
+
+const showMore = ref(false);
+let selectedIngredients = ref<any[]>([])
 
 const handleRating = async(value: number) => {
-  try {
-    props.recipe.rating = value
-    await recipeStore.update(props.recipe._id, props.recipe.rating)   
-  } catch (e) {
+  try{
+    await $fetch(`/recipe/${props.recipe!._id}`, {method: "PUT", body: {rating: value}})
+  }catch(e){
     console.log(e)
   }
 }
+
+const toggleIngredient = (ingredient: { id: string; }) => {
+  const index = selectedIngredients.value.findIndex(
+    (item) => item.id === ingredient.id
+  );
+  if (index === -1) {
+    selectedIngredients.value.push(ingredient);
+  } else {
+    selectedIngredients.value.splice(index, 1);
+  }
+};
+
+const isSelected = (ingredient: { id: string; }) => {
+  return selectedIngredients.value.some((item) => item.id === ingredient.id);
+}
+
+
+const toggleShowMore = () => {
+  showMore.value = !showMore.value
+};
+
 </script>
 <template>
-  <div class="p-8">
-   <h1 class="text-3xl lg:text-6xl uppercase font-bold my-2"> {{ recipe.title }}</h1>
-   <hr v-if="recipe.title" class="bg-gray-500 lg:bg-gray-900"/>
-   <h1 class="text-4xl my-2"> {{ recipe.description }}</h1>
-   <StarRating v-if="recipe._id" @rating="handleRating" :currentRating="recipe.rating"/>
+  <div class="px-8 py-32 md:px-4 md:py-16 lg:px-8 lg:py-16">
+    <h1 v-if="recipe.title" class="text-3xl lg:text-5xl uppercase font-lora font-medium my-2"> {{ recipe.title }} </h1>
+    <hr v-if="recipe.title" class="bg-gray-500 lg:bg-gray-900"/>
+    <p class="text-3xl italic my-2 line-clamp-3 font-lora italics" :class="{ 'line-clamp-none': showMore }"> {{ recipe.description }}</p>
+    <button v-if="recipe.description.trim().length >= 200" @click="toggleShowMore" class="block text-xl text-gray-400 italic">{{ showMore ? 'less' : 'more' }}</button>
+    <StarRating v-if="recipe._id" @rating="handleRating" :currentRating="recipe.rating"/>
 
-   <div v-if="recipe.image" class=" w-full lg:bg-auto bg-center lg:bg-center bg-no-repeat bg-gray-200 h-48 lg:h-96 my-2" :style="{ backgroundImage: `url('${recipe.image}')`, minHeight: '200px' }"/>
-    <div class="my-4 flex justify-between">
+    <div v-if="recipe.image" class="w-full bg-center lg:bg-cover bg-no-repeat bg-gray-200 h-48 lg:h-96 my-2" :style="{ backgroundImage: `url('${recipe.image}')`, minHeight: '200px' }"/>
+    <div class="my-4 flex justify-between font-mulish">
       <div>
-        <a :href="recipe.url" target="_blank" class="capitalize font-medium text-xl link">{{ recipe.source }}</a>
-        <h2 v-if="recipe.yields" class="capitalize my-1 text-lg"> serves: {{ recipe.yields }}</h2>
+        <a :href="recipe.url" target="_blank" class="capitalize text-base link font-semibold">{{ recipe.source }}</a>
+        <h2 v-if="recipe.yields" class="capitalize my-1 text-md"> serves: {{ recipe.yields }}</h2>
       </div>
 
       <div>
-        <ul class="flex gap-x-4">
+        <ul class="flex gap-x-4 font-mulish">
           <li v-for="item in recipe.cookingtime" :key="item.id">
-            <h1 class="text-lg font-medium uppercase">{{ item.title }}</h1>
+            <h1 class="text-base font-medium uppercase">{{ item.title }}</h1>
             <span v-if="item.hours" class="">{{ item.hours }} hrs </span>
             <span v-if="item.minutes" class="">{{ item.minutes }} mins</span>
           </li>
         </ul>
       </div>
     </div>
-    <div class="hidden lg:flex lg:justify-between lg:gap-x-8">
-      <ul class=" p-2 w-1/2 space-y-4" >
-        <li v-for="item in recipe.ingredients">{{ item }}</li>
+    <div class="hidden md:flex md:justify-between lg:flex lg:justify-between lg:gap-x-6 font-mulish">
+      <ul class="py-2 basis-2/5 space-y-4" >
+        <li v-for="ingredient in recipeIngredients" :key="ingredient.id">
+          <span @click="toggleIngredient(ingredient)" :class="{ 'selected': isSelected(ingredient!) }">
+            <strong>{{ convertToFraction(ingredient.quantity) }} {{ ingredient.unit }}</strong> {{ parseVulgars(ingredient.note) }}
+          </span>
+        </li>
       </ul>
-      <ul class=" w-1/2 p-2 space-y-4">
+      <ol class="basis-3/5 py-2 space-y-4">
         <li v-for="step in recipe.steps">{{ step }}</li>
-      </ul>
-      <!-- <client-only placeholder="loading...">
-        <HtmlView :html="`${htmlString}`" :tags="['p', 'b', 'i', 'h1', 'strong', 'li']"/>
-      </client-only> -->
+      </ol>
     </div>
     <client-only>
       <iframe 
