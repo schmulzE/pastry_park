9e59897bfe81bfae9f21bd1c406f3fee02f36ed6
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-10-01 15:40:19 +0100

    removed unwanted composables and middleware

--- composables/useAuthUser.ts
+++
@@ -1,7 +0,0 @@
-import type { UserWithoutPassword } from "~~/types";
-// import type { User } from "~~/types";
-
-export const useAuthUser = () => {
-    return useState<UserWithoutPassword | null>("user", () => null);
-    // return useState<User | null>("user", () => null);
-};
