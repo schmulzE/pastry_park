46ddbf2aeea422bc9bdcedf09bb7a64861792088
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-10-02 09:47:45 +0100

    Implemented recipe deletion endpoint -Added event handler for recipe deletion by ID -Integrated with MongoDB using Mongoose -Implemented error handling for deletion process -Returned success message upon successful deletion -Used TypeScript for improved type safety -Utilized Nuxt's defineEventHandler for server-side logic

--- server/routes/recipe/[id].delete.ts
+++ server/routes/recipe/[id].delete.ts
@@ -1,16 +1,16 @@
 import { Recipe } from "~~/server/models/recipe";
 
 export default defineEventHandler(async (event) => {
-    // Get id from params
-    const id = event.context.params!.id;
+	// Get id from params
+	const id = event.context.params!.id;
 
-    // Remove recipe
-    try {
-        await Recipe.findByIdAndDelete(id);
-        return { message: "Recipe removed" };
-    } catch (e :any) {
-        throw createError({
-            message: e.message,
-        });
-    }
+	// Remove recipe
+	try {
+		await Recipe.findByIdAndDelete(id);
+		return { message: "Recipe removed" };
+	} catch (e :any) {
+		throw createError({
+			message: e.message,
+		});
+	}
 });
