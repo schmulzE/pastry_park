517dde3c0c6e6b2cee9515f6bc3d18e04a1dbeb0
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-10-02 11:46:13 +0100

    Add home page layout with Navbar, HeroSection, InfoSections, Features, and Footer components

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,26 @@
 <script lang="ts" setup>
-const currentUser = useAuthUser();
+
 </script>
 
 <template>
-    <div>
-        <PageTitle title="Home" />
-        <PageDescription description="Simple authentification example using Nuxt3" />
-        <PageUser :user="currentUser" />
-    </div>
+  <NuxtLayout name="default">
+    <template #main>
+      <Navbar/>
+      <HeroSection/>
+      <InfoSection 
+      title="Make recipes your own" 
+      description="With pastry park recipe editor, you can easily edit recipes, save adjustments to ingredients, and add additional steps or tips to your preparation." 
+      imageOne="https://images.pexels.com/photos/33715/cake-pops-pastries-cake-sweet.jpg?auto=compress&cs=tinysrgb&w=600" 
+      imageTwo="https://images.pexels.com/photos/33715/cake-pops-pastries-cake-sweet.jpg?auto=compress&cs=tinysrgb&w=600"
+      />
+      <Features/>
+      <InfoSection 
+      title="Cook from your favorite device" 
+      description="pastry park stores your recipes in the Cloud so you can access them on any device through our website."
+      imageOne="https://images.pexels.com/photos/33715/cake-pops-pastries-cake-sweet.jpg?auto=compress&cs=tinysrgb&w=600" 
+      imageTwo="https://images.pexels.com/photos/33715/cake-pops-pastries-cake-sweet.jpg?auto=compress&cs=tinysrgb&w=600"
+      />
+      <Footer/>
+    </template>
+  </NuxtLayout>
 </template>
