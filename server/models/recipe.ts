bbc9cb93950c7bf1220d1678220d62d5f82592ca
wisdom eshiet <wisdomeshiet65@gmail.com>
2024-10-02 09:58:52 +0100

    Defined Recipe schema and model with enhanced features -Created RecipeDocument interface for TypeScript support -Implemented mongoose Schema for Recipe with detailed fields -Added timestamps for creation and update tracking -Implemented pre-save hook for logging recipe details before saving -Added post-save hook for error handling and successful save logging -Defined CookingTime interface for structured cooking time data -Included user reference in schema for recipe ownership -Set up category field as required- Export Recipe model for use in other parts of the application

--- server/models/recipe.ts
+++ server/models/recipe.ts
@@ -1,24 +1,61 @@
 import { Schema, model } from "mongoose";
 
-const RecipeSchema = new Schema(
-    {
-        title: String,
-        description: String,
-        steps: String,
-        ingredients: String,
-        video_url: String,
-        image: String,
-        url: String,
-        cookingtime: Array,
-        source: String,
-        yield: String,
-        category: String,
-        rating: Number,
-        createdBy: { type: Schema.Types.ObjectId, ref: 'User' }, // Reference to User model
-    },
-    {
-        timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
-    }
+interface CookingTime{
+	id: string,
+	title: string,
+	hour: number,
+	minute: number,
+}
+
+export interface RecipeDocument extends Document {
+  title: string,
+	description: string,
+	steps: string,
+	ingredients: string,
+	video_url: string,
+	image: string,
+	url: string,
+	cookingtime: CookingTime[],
+	source: string,
+	yields: string,
+	category: string,
+	rating: number,
+	createdBy: string,
+	createdAt?: string,
+}
+
+const RecipeSchema = new Schema({
+	title: String,
+	description: String,
+	steps: Array,
+	ingredients: Array,
+	video_url: String,
+	image: String,
+	url: String,
+	cookingtime: Array,
+	source: String,
+	yields: String,
+	category: {type: String, required: true},
+	rating: Number,
+	created_by: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to User model
+},
+{
+	timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+}
 );
 
-export const Recipe = model("Recipe", RecipeSchema);
+	RecipeSchema.pre('save', function(next) {
+		console.log('Saving recipe:', this);
+		next();
+	});
+
+	RecipeSchema.post('save', function(error: any, doc: any, next: () => void) {
+		if (error) {
+			console.error('Error saving recipe:', error);
+		} else {
+			console.log('Recipe saved:', doc);
+		}
+		next();
+	});
+
+export const Recipe = model<RecipeDocument>("Recipe", RecipeSchema);
